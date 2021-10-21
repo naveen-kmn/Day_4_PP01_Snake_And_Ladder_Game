65dae32a42ff9ca7389d38f419f1af6d86854215
Naveen KM <kmnaveenmech@gmail.com>
2021-10-21 22:47:11 +0530

    Added: Welcome message is added

--- src/com/blp/snakeandladdergame/SnakeAndLadderGame.java
+++ src/com/blp/snakeandladdergame/SnakeAndLadderGame.java
@@ -4,7 +4,6 @@ public class SnakeAndLadderGame
 {
     public static void main(String[] args)
     {
-        int position = 0;
-        System.out.println("Position of player = " + position);
+        System.out.println("Welcome to Snake and Ladder Game");
     }
 }
